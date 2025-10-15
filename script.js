c93b8958a808e0d4e1d6c0f595b6077a2cfed6a1
Joey Bảo Châu <n23dcpt008@student.ptithcm.edu.vn>
2025-10-15 09:37:47 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Scroll animation
-const pops = document.querySelectorAll('.pop');
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) entry.target.classList.add('show');
-  });
-});
-pops.forEach(pop => observer.observe(pop));
